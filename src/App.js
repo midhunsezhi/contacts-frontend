20a8300597cd5bcfe9c1e75a6f10298b3ca8b529
Midhun Dandamudi <midhunsezhi@gmail.com>
2017-08-26 12:48:54 -0700

    update the app to have the ability to create contact

--- src/App.js
+++ src/App.js
@@ -22,6 +22,14 @@ class App extends Component {
 
     ContactsAPI.remove(contact);
   }
+
+  createContact = (contact) => {
+    ContactsAPI.create(contact).then((contact) => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([ contact ])
+      }))
+    })
+  }
   render() {
     return (
       <div className='app'>
@@ -32,7 +40,14 @@ class App extends Component {
           />
         )} /> 
         
-        <Route path='/create' component={CreateContact} />
+        <Route path='/create' render={ ({history}) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact);
+              history.push('/')
+            }}
+          />
+        )} />
       </div>
     )
   }
